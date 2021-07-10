8c58132d5bb378fdf1b20abfd08e543de53b4ba7
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-07-10 10:18:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -64,45 +64,96 @@ const galleryItems = [
   },
 ];
 
+const refs = {
+  galleryContainer: document.querySelector('.js-gallery'),
+  modal: document.querySelector('.lightbox'),
+  backdrop: document.querySelector('.lightbox__overlay'),
+  modalImage: document.querySelector('.lightbox__image'),
+  closeBtn: document.querySelector('.lightbox__button')  
+}
 
-const galleryContainer = document.querySelector('.js-gallery');
-// console.log(galleryContainer);
-// console.dir(galleryContainer);
+//console.log(refs.closeBtn);
+
 const cardsMarkup = createCards(galleryItems);
-console.log(cardsMarkup);
-galleryContainer.insertAdjacentHTML('afterbegin', cardsMarkup)
+refs.galleryContainer.insertAdjacentHTML('afterbegin', cardsMarkup);
+
+refs.galleryContainer.addEventListener('click', openModal);
+refs.closeBtn.addEventListener('click', closeModal);
+refs.backdrop.addEventListener('click', closeBackdrop);
+
+function closeBackdrop(event) {
+
+  if (event.target === event.currentTarget) {
+    closeModal(event);
+    console.log("backdropOverlay")
+  }
+
+  console.log(event.target);
+  console.log(event.currentTarget)
+}
+
+function openModal(event) {
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  window.addEventListener('keydown', closeEscModal);
+  window.addEventListener('keypress', slidePictures);
+
+  // console.log(event.target);
+  // console.log(event.target.dataset.alt);
+
+  event.preventDefault();
+  addClassListIsOpen();
+  
+  addBigSizeSorce(event.target.dataset.source, event.target.dataset.alt );
+}
+
+function slidePictures(event) {
+  console.log(event);
+}
+
+function closeEscModal(event) {
+   console.log(event);
+  const ESC_KEY_CODE = 'Escape';
+  if (event.code === ESC_KEY_CODE) {
+    closeModal();
+  }
+}
+
+function addBigSizeSorce(src, alt) {
+  refs.modalImage.src = src;
+  refs.modalImage.alt = alt;
+}
+
+function addClassListIsOpen() {
+  refs.modal.classList.add('is-open');
+}
+
+function closeModal(event) {
+  refs.modal.classList.remove('is-open');
+  window.removeEventListener('keydown', closeModal);
+  refs.modalImage.src = "";
+  refs.modalImage.alt = "";
+}
 
 function createCards(galleryItems) {
 
-  return galleryItems
- 
+  return galleryItems 
     .map(item => {
       return `<li class="gallery__item">
     <a class="gallery__link" href="${item.original}">
-    </a>
     <img
       class="gallery__image"
       src="${item.preview}"
       data-source="${item.original}"
       alt="${item.description}"
     />
+    </a>
     </li>`;
     }).join('');
+  
 }
-
-{/* <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
-  </a>
-</li> */}
 
 
 
